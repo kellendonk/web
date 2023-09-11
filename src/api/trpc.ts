28413c11ddb4d5480d6a6feac9ac8a000ac8057b
Josh Kellendonk <joshkellendonk@gmail.com>
2023-09-10 19:22:23 -0600

    hyphenate annotation names

--- src/api/trpc.ts
+++ src/api/trpc.ts
@@ -4,8 +4,8 @@ import { tracer } from './powertools';
 const trpc = initTRPC.create();
 
 const tracerMiddleware = trpc.middleware(async (opts) => {
-  tracer.putAnnotation('trpc.path', opts.path);
-  tracer.putAnnotation('trpc.type', opts.type);
+  tracer.putAnnotation('trpc-path', opts.path);
+  tracer.putAnnotation('trpc-type', opts.type);
 
   const start = Date.now();
   const res = await opts.next(opts);
@@ -13,9 +13,9 @@ const tracerMiddleware = trpc.middleware(async (opts) => {
   tracer.putMetadata('trpc.duration', Date.now() - start);
 
   if (res.ok === true) {
-    tracer.putAnnotation('trpc.status', 'ok');
+    tracer.putAnnotation('trpc-status', 'ok');
   } else {
-    tracer.putAnnotation('trpc.status', 'error');
+    tracer.putAnnotation('trpc-status', 'error');
     tracer.putMetadata('trpc.error', res.error);
   }
 
